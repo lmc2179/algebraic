a752bf4d7b3e9ded500aa6ddd2f95fdb86e5c387
lmc2179 <louiscialdella@gmail.com>
2016-02-22 00:06:06 -0500

    refactored test suite to prepare for adding more statistics

--- tests.py
+++ tests.py
@@ -1,63 +1,84 @@
 import unittest
 from algebraic_statistic import Mean
+import random
 
-class MeanTest(unittest.TestCase):
-    def test_mean_calculation_correctness(self):
-        dataset = [1, 3, 4, -10, 2334, 3, 100]
-        m = Mean(dataset)
-        true_mean = 1.0 * sum(dataset) / len(dataset)
-        self.assertEqual(true_mean, m.get_mean())
-        self.assertEqual(len(dataset), m.get_n())
+class AbstractAlgebraicStatisticTest(unittest.TestCase):
+    STATISTIC_CLS = None
+
+    def _generate_data_set(self, size):
+        raise NotImplementedError
+
+    def _generate_data_sets(self, sizes):
+        return [self._generate_data_set(s) for s in sizes]
+
+    def _partition_data_set(self, data_set, slice_sizes):
+        raise NotImplementedError
+
+    def _assert_equal(self, s1, s2):
+        raise NotImplementedError
 
     def test_mean_merge_correctness(self):
-        d1, d2 = [1, 3, 4] ,[-10, 2334, 3, 100]
+        d1, d2 = self._generate_data_sets([3, 4])
         merged_dataset = d1 + d2
-        merged_dataset_m = Mean(merged_dataset)
-        m1, m2 = Mean(d1), Mean(d2)
+        merged_dataset_m = self.STATISTIC_CLS(merged_dataset)
+        m1, m2 = self.STATISTIC_CLS(d1), self.STATISTIC_CLS(d2)
         merged_algebraic_m = m1 | m2
-        self.assertEqual(merged_dataset_m.get_mean(), merged_algebraic_m.get_mean())
-        self.assertEqual(merged_dataset_m.get_n(), merged_algebraic_m.get_n())
+        self._assert_equal(merged_algebraic_m, merged_dataset_m)
 
     def test_mean_eq(self):
-        d1, d2 = [1, 3, 4] ,[-10, 2334, 3, 100]
-        self.assertTrue(Mean(d1) == Mean(d1))
-        self.assertFalse(Mean(d1) == Mean(d2))
+        d1, d2 = self._generate_data_sets([3, 4])
+        self.assertTrue(self.STATISTIC_CLS(d1) == self.STATISTIC_CLS(d1))
+        self.assertFalse(self.STATISTIC_CLS(d1) == self.STATISTIC_CLS(d2))
 
+class SemigroupStatisticTest(AbstractAlgebraicStatisticTest):
+    def test_associativity(self):
+        d1, d2, d3 = self._generate_data_sets([3, 4, 5])
+        m1, m2, m3 = self.STATISTIC_CLS(d1), self.STATISTIC_CLS(d2), self.STATISTIC_CLS(d3)
+        merged_algebraic_m_left = (m1 | m2) | m3
+        merged_algebraic_m_right = m1 | (m2 | m3)
+        self._assert_equal(merged_algebraic_m_left, merged_algebraic_m_right)
+
+class MonoidStatisticTest(SemigroupStatisticTest):
+    def test_identity(self):
+        dataset = self._generate_data_set(7)
+        m = self.STATISTIC_CLS(dataset) | self.STATISTIC_CLS.get_identity()
+        m_gold = self.STATISTIC_CLS(dataset)
+        self.assertEqual(m, m_gold)
+
+class GroupStatisticTest(MonoidStatisticTest):
     def test_inverse(self):
-        d1, d2 = [1, 3, 4] ,[-10, 2334, 3, 100]
-        m1, m2 = Mean(d1), Mean(d2)
+        d1, d2 = self._generate_data_sets([3, 4])
+        m1, m2 = self.STATISTIC_CLS(d1), self.STATISTIC_CLS(d2)
         merged_algebraic_m = m1 | m2 | -m2
-        self.assertEqual(m1.get_mean(), merged_algebraic_m.get_mean())
-        self.assertEqual(m1.get_n(), merged_algebraic_m.get_n())
+        self._assert_equal(m1, merged_algebraic_m)
 
     def test_inverse_to_identity(self):
-        dataset = [1, 3, 4, -10, 2334, 3, 100]
-        m = Mean(dataset) - Mean(dataset)
-        m_ident = Mean.get_identity()
-        self.assertEqual(m.get_mean(), m_ident.get_mean())
-        self.assertEqual(m.get_n(), m_ident.get_n())
+        dataset = self._generate_data_set(7)
+        m = self.STATISTIC_CLS(dataset) - self.STATISTIC_CLS(dataset)
+        m_ident = self.STATISTIC_CLS.get_identity()
+        self._assert_equal(m, m_ident)
 
     def test_sub(self):
-        d1, d2 = [1, 3, 4] ,[-10, 2334, 3, 100]
-        m1, m2 = Mean(d1), Mean(d2)
+        d1, d2 = self._generate_data_sets([3, 4])
+        m1, m2 = self.STATISTIC_CLS(d1), self.STATISTIC_CLS(d2)
         merged_algebraic_m = m1 | m2 - m2
-        self.assertEqual(m1.get_mean(), merged_algebraic_m.get_mean())
-        self.assertEqual(m1.get_n(), merged_algebraic_m.get_n())
+        self._assert_equal(m1, merged_algebraic_m)
 
-    def test_identity(self):
-        dataset = [1, 3, 4, -10, 2334, 3, 100]
-        m = Mean(dataset) | Mean.get_identity()
+class MeanTest(GroupStatisticTest):
+    STATISTIC_CLS = Mean
+    def _generate_data_set(self, size):
+        return [random.randint(-1000, 1000) for i in range(size)]
+
+    def _assert_equal(self, s1, s2):
+        self.assertAlmostEqual(s1.get_mean(), s2.get_mean(), places=6)
+        self.assertEqual(s1.get_n(), s2.get_n())
+
+    def test_statistic_calculation_correctness(self):
+        dataset = self._generate_data_set(7)
+        m = self.STATISTIC_CLS(dataset)
         true_mean = 1.0 * sum(dataset) / len(dataset)
         self.assertEqual(true_mean, m.get_mean())
         self.assertEqual(len(dataset), m.get_n())
-
-    def test_associativity(self):
-        d1, d2, d3 = [1, 3, 4] ,[-10, 2334, 3, 100], [-10, 20, 2200,3,3,4]
-        m1, m2, m3 = Mean(d1), Mean(d2), Mean(d3)
-        merged_algebraic_m_left = (m1 | m2) | m3
-        merged_algebraic_m_right = m1 | (m2 | m3)
-        self.assertEqual(merged_algebraic_m_left.get_mean(), merged_algebraic_m_right.get_mean())
-        self.assertEqual(merged_algebraic_m_left.get_n(), merged_algebraic_m_right.get_n())
 
 if __name__ == '__main__':
     unittest.main()
